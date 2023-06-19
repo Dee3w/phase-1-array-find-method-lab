0e6857ba20a4a5ab993278140da66a593936a3dd
revolutionartist <joydanah17@gmail.com>
2023-06-19 22:02:22 +0300

    function to meet requirements

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W", team: "Denver Broncos"},
+    { year: "2014", result: "N/A", team: "Some Other Team"},
+    { year: "2013", result: "L", team: "Denver Broncos"},
+
+  ];
+
+function superbowlWin(arr) {
+    const win = arr.find(obj => obj.result === "W"); 
+    return win ? win.year : undefined;
+  }
+  console.log(superbowlWin(record));
+
+  
